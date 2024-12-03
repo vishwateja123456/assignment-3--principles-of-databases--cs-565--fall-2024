762aed8aa3da1ff52e91f8f0c9a4ecc226a63b40
Roy Vanegas <roy@thecodeeducators.com>
2024-12-03 18:56:21 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require(`express`);
+const app = express();
+const nunjucks = require(`nunjucks`);
+const bodyParser = require(`body-parser`);
+const mongoDB = require(`mongodb-legacy`); // https://www.npmjs.com/package/mongodb-legacy
+const mongoClient = mongoDB.MongoClient;
+const HOST = `localhost`;
+const dbPort = `27017`;
+const dbURL = `mongodb://${HOST}`;
+const dbName = `project`;
+const dbCollection = `users`;
+const PORT = 3000;
+const port = (process.env.PORT || PORT);
+const colors = {
+    reset: `\x1b[0m`,
+    red: `\x1b[31m`,
+    green: `\x1b[32m`,
+    yellow: `\x1b[33m`,
+};
+
+let db;
